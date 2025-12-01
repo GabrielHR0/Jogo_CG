89dc00020fe16b8d3cdafc6f5be6ed6f0fd83a34
AlexMeds <cristianalex055@gmail.com>
2025-12-01 16:58:21 -0300

    corrigindo cutscenes

--- scenes/Menu/main_menu.gd
+++ scenes/Menu/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/Cutscenes/cutscene_1.tscn")
 
 
 func _on_credits_button_pressed() -> void:
